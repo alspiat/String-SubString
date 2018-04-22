33fa623e0d7bcf371198700322cbe26997e1ba49
alspiat <alspiat@icloud.com>
2018-04-23 00:01:29 +0300

    Update ViewController.m

--- String-SubString/String-SubString/ViewController.m
+++ String-SubString/String-SubString/ViewController.m
@@ -20,9 +20,11 @@
     
     Tagger *tagger = [[Tagger alloc] initWithString:@"Prime Minister <Narendra Modi> tweeted a <link> to the speech (Human Resource Development Minister Smriti Irani) <made> in the Lok Sabha during the ((debate) on the ongoing JNU row) and the suicide of Dalit scholar Rohith Vemula at the [Hyderabad Central University]."];
     NSLog(@"%@", [tagger splitStringByTags]);
+    [tagger release];
     
     tagger = [[Tagger alloc] initWithString:@"Lorem <(ipsum [dolor <sit] amet), consectetur adipiscing elit>. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet>. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget (nulla. Class aptent <taciti [sociosqu ad] litora torquent per conubia> nostra), per inceptos himenaeos."];
     NSLog(@"%@", [tagger splitStringByTags]);
+    [tagger release];
     
 }
 
